fee1d6e87ef689fe32ede65fc501a8abaff5b678
Eric Phelps <Eric.Phelps@tlgcohort.com>
2023-07-27 12:41:09 -0400

    lab 2.2 complete

--- Lab2.2/src/com/algorithms/BubbleSort.java
+++ Lab2.2/src/com/algorithms/BubbleSort.java
@@ -5,14 +5,55 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
+        // The input array that needs to be sorted
         int[] input = {22, 15, 13, 9, 6, 3, 1};
-        System.out.println(Arrays.toString(input));
+
+        // Display the original input array
+        System.out.println("Original Array: " + Arrays.toString(input));
+
+        // Call the sort method to sort the input array
         sort(input);
-        System.out.println(Arrays.toString(input));
+
+        // Display the sorted array
+        System.out.println("Sorted Array: " + Arrays.toString(input));
     }
 
-    public static void sort(int[] numbers) {
-        // TODO: implement bubble sort pseudocode
-    }
+    public static void sort(int[] numbers) { //sorts a given array in ascending order
+        // Get the length of the input array
+        int n = numbers.length;
 
+        // A flag to indicate whether any swaps occurred in the current pass
+        boolean swapped;
+
+        // Outer loop to iterate over the array elements
+        // It runs (n-1) times, where n is the number of elements in the array
+        for (int i = 0; i < n - 1; i++) {
+            // Initialize the 'swapped' flag as false at the start of each pass
+            swapped = false;
+
+            // Inner loop to compare adjacent elements and swap if necessary
+            // It runs from the first element to the (n-i-1)-th element
+            // Since the largest element will be "bubbled" to the end in each pass,
+            // there is no need to compare the already sorted elements
+            for (int j = 0; j < n - i - 1; j++) {
+                // Compare the current element with its neighbor
+                if (numbers[j] > numbers[j + 1]) {
+                    // Swap the elements because they are in the wrong order
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+
+                    // Set the 'swapped' flag to true as a swap occurred in this pass
+                    swapped = true;
+                }
+            }
+
+            // If no swaps occurred in the current pass, the array is already sorted
+            // No need to continue further iterations, break out of the loop
+            if (!swapped) {
+                break;
+            }
+        }
+    }
 }
+
--- Lab2.2/test/com/algorithms/BubbleSortTest.java
+++ Lab2.2/test/com/algorithms/BubbleSortTest.java
@@ -1,7 +1,34 @@
+//all tests are run with Junit 5
 package com.algorithms;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 public class BubbleSortTest {
 
-    // TODO: Create test methods for bubble sort implementation.
+    @Test
+    public void testBubbleSortAscending() {
+        int[] input = {22, 15, 13, 9, 6, 3, 1};
+        BubbleSort.sort(input);
+        int[] expected = {1, 3, 6, 9, 13, 15, 22};
+        Assertions.assertArrayEquals(expected, input);
+    }
 
+    @Test
+    public void testBubbleSortAlreadySorted() {
+        int[] input = {1, 2, 3, 4, 5};
+        BubbleSort.sort(input);
+        int[] expected = {1, 2, 3, 4, 5};
+        Assertions.assertArrayEquals(expected, input);
+    }
+
+    @Test
+    public void testBubbleSortEmptyArray() {
+        int[] input = {};
+        BubbleSort.sort(input);
+        int[] expected = {};
+        Assertions.assertArrayEquals(expected, input);
+    }
 }
